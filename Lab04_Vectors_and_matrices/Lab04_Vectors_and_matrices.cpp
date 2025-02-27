43c92a0b83677191ba12f63ca26c6db040336b96
Zul <146729926+Zulfaqaar@users.noreply.github.com>
2025-02-27 15:42:29 +0000

    4.1 - 4.3.1

--- Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
+++ Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
-
 // Include the glm library
 #include <glm/glm.hpp>
 #include <glm/gtx/io.hpp>
 
 int main() {
-    
+    // Vectors
+    printf("Vectors and Matrices\n");
+    printf("--------------------------------------------------\n");
+
+    // Defining vectors
+    glm::vec3 a, b;
+    a = glm::vec3(3.0f, 0.0f, 4.0f);
+    b[0] = 1.0f, b[1] = 2.0f, b[2] = 3.0f;
+
+    printf("Defining vectors:\n");
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+
+    // Vector length
+    printf("\nVector length:\n");
+    printf("length(a) = %0.3f\n", glm::length(a));
+    printf("length(b) = %0.3f\n", glm::length(b));
+
+    // Normalising vectors
+    glm::vec3 aHat, bHat;
+    aHat = glm::normalize(a);
+    bHat = b / glm::length(b);
+
+    printf("\nNormalising vectors:\n");
+    std::cout << "aHat = " << aHat << std::endl;
+    std::cout << "bHat = " << bHat << std::endl;
+    printf("length(aHat) = %0.3f\n", glm::length(aHat));
+    printf("length(bHat) = %0.3f\n", glm::length(bHat));
+
+    // Arithmetic operations on vectors
+    printf("\nArithmetic operations on vectors:\n");
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "a - b = " << a - b << std::endl;
+
     return 0;
 }
