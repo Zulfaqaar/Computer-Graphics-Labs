58c8dcb9f7eaea67f39f9297dc6f9d8d241e1a0d
Zul <146729926+Zulfaqaar@users.noreply.github.com>
2025-03-12 14:18:54 +0000

    7.1 - 7.4

--- common/camera.hpp
+++ common/camera.hpp
@@ -17,14 +17,23 @@ public:
     glm::vec3 eye;
     glm::vec3 target;
     glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 right = glm::vec3(1.0f, 0.0f, 0.0f);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
 
     // Transformation matrices
     glm::mat4 view;
     glm::mat4 projection;
+
+    // Camera Euler angles
+    float yaw = Maths::radians(-90.0f);
+    float pitch = 0.0f;
+    float roll = 0.0f;
 
     // Constructor
     Camera(const glm::vec3 eye, const glm::vec3 target);
 
     // Methods
     void calculateMatrices();
+    void calculateCameraVectors();
 };
